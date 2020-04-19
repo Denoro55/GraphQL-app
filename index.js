2e56472f7cea7552963c09989d5aaee594351428
Denis Chertenko <denis.chertenko@mail.ru>
2020-04-19 23:25:06 +0300

    Инициализация rest-api приложения

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sequelize = require('./utils/database');
+
+// routes
+const todoRoutes = require('./routes/todo');
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use('/api/todo', todoRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile('/index.html');
+});
+
+async function start() {
+    try {
+        await sequelize.sync();
+        app.listen(PORT, () => {
+            console.log('server is listening on port ', PORT);
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();
